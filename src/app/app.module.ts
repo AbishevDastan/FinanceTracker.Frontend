ce666e8ee9238f6a641d7227849ccfc8976b42c6
Dastan Abishev <abish.ev@mail.ru>
2023-12-14 13:53:37 +0100

    Authentication/authorization for user, routing, header implemented.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './components/pages/registration/registration.component';
+import { AuthenticationInterceptor } from './interceptors/auth.interceptor';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent }
+];
+
+const AUTHENTICATION_INTERCEPTOR = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: AuthenticationInterceptor,
+  multi: true
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, {scrollPositionRestoration: 'enabled'}),
+    HttpClientModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,92 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+import { UserProvider } from '../providers/user.provider';
+import { User } from '../models/user/user';
+import { RegisterUser } from '../models/user/register-user';
+import { TokenModel } from '../models/user/token-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private url = "User";
+
+  constructor(private httpClient: HttpClient,
+      private userProvider: UserProvider) { }
+
+  getUsers() : Observable<Array<User>> {
+    return this.httpClient.get<Array<User>>(`${environment.apiUrl}/${this.url}`);
+  }
+
+  getUser(userId?: number) : Observable<User> {
+    return this.httpClient.get<User>(`${environment.apiUrl}/${this.url}/${userId}`);
+  }
+
+  register(email: string, name: string, surname: string, password: string, confirmPassword: string) {
+    return this.httpClient.post<RegisterUser>(`${environment.apiUrl}/${this.url}/register`, {
+      email,
+      name,
+      surname,
+      password,
+      confirmPassword
+    });
+  }
+
+  searchUsers(searchText: string) : Observable<Array<User>> {
+    return this.httpClient.get<Array<User>>(`${environment.apiUrl}/${this.url}/${searchText}/search`)
+  }
+
+  private static setToken(tokenModel: TokenModel | null): void {
+    if (tokenModel) {
+      localStorage.setItem('token', tokenModel.token);
+      localStorage.setItem('token-exp', tokenModel.expiresAt.toString());
+    } else {
+      this.removeLocalStorageItems();
+    }
+  }
+
+  private static removeLocalStorageItems(): void {
+    localStorage.removeItem('token');
+    localStorage.removeItem('token-exp');
+  }
+
+  login(email: string, password: string) {
+    return this.httpClient
+      .post(`${environment.apiUrl}/${this.url}/login`, { email, password })
+      .pipe(
+        tap((tokenResponse: any) => {
+              UserService.setToken(tokenResponse);
+              this.isAuthenticated();
+        }),
+      );
+  }
+
+  logout(): void {
+    UserService.setToken(null);
+    this.userProvider.currentUser = null;
+  }
+
+  isAuthenticated(): boolean {
+    return this.token !== null;
+  }
+
+  get token(): string|null {
+    const expDate = new Date(localStorage.getItem('token-exp') as string);
+    if (new Date() > expDate) {
+      this.logout();
+      return null;
+    }
+    return localStorage.getItem('token');
+  }
+
+  get expiredAt(): Date {
+    const expDate = localStorage.getItem('token-exp');
+
+    return expDate
+      ? new Date(expDate)
+      : new Date();
+  }
+}
